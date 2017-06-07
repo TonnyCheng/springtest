8c0364345036d6f8173d356c1a8058d11b1cfed4
TonnyCheng <645733908@qq.com>
2017-06-07 14:50:10 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Created by cyb on 2017/6/7.
+ */
+@RestController
+public class Controller {
+    @RequestMapping("/hello")
+    public String test(){
+        return "hello";
+    }
+}
